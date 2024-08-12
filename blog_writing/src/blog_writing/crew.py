34687c31317de2da8fad82b9472bf858259b22b4
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-12 08:53:23 +0500

    setup crew.py (agents, tasks, tool, llm)

--- blog_writing/src/blog_writing/crew.py
+++ blog_writing/src/blog_writing/crew.py
@@ -1,5 +1,15 @@
 from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
+from crewai.project import CrewBase, agent, crew, task, llm, tool
+from crewai_tools import YoutubeChannelSearchTool
+
+from langchain_huggingface import HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+hf_ep = os.environ['HUGGINGFACE_ENDPOINT']
+
 
 @CrewBase
 class BlogWritingCrew():
@@ -7,19 +17,42 @@ class BlogWritingCrew():
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True
+	@tool
+	def yt_tool(self):
+		return YoutubeChannelSearchTool(
+			youtube_channel_handle=os.environ['CHANNEL_HANDLE'],
+			config={
+				"llm": {
+					"provider": "huggingface",
+					"config": {
+						"model": hf_ep
+					}
+				},
+				"embedder": {
+					"provider": "huggingface",
+					"config": {
+						"model": hf_ep
+					}
+				}
+			}
 		)
 
 	@agent
-	def reporting_analyst(self) -> Agent:
+	def blog_researcher(self) -> Agent:
 		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
+			config=self.agents_config['researcher'],
+			memory=True,
+			allow_delegation=True,
+			verbose=True,
+		)
+
+	@agent
+	def blog_writer(self) -> Agent:
+		return Agent(
+			config=self.agents_config['blog_writer'],
+			memory=True,
+			allow_delegation=True,
+			verbose=True,
 		)
 
 	@task
@@ -29,10 +62,20 @@ class BlogWritingCrew():
 		)
 
 	@task
-	def reporting_task(self) -> Task:
+	def writing_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+			config=self.tasks_config['writing_task'],
+			output_file='new-blog-post.md'
+		)
+	
+	@llm
+	def llm(self):
+		return HuggingFaceEndpoint(
+			repo_id=hf_ep,
+			task="text-generation",
+			max_new_tokens=512,
+			do_sample=False,
+			repetition_penalty=1.03
 		)
 
 	@crew
@@ -43,5 +86,8 @@ class BlogWritingCrew():
 			tasks=self.tasks, # Automatically created by the @task decorator
 			process=Process.sequential,
 			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+			memory=True,
+			cache=True,
+			max_rpm=100,
+			share_crew=False,
 		)
