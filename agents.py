75fdacf6fb7d6741705d5d3ddba1b0c486abebb0
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-12 07:31:41 +0500

    blog researcher and writer agents

---
+++ agents.py
@@ -0,0 +1,28 @@
+from crewai import Agent
+
+
+blog_researcher = Agent(
+    role='Blog Researcher from YouTube Videos',
+    goal='Get the relevant video content for topic {topic} from YT channel.',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "Expert in understanding videos in Data Science, Machine Learning and GenAI, and providing suggestions."
+    ),
+    tools=[],
+    allow_delegation=True,
+)
+
+blog_writer = Agent(
+    role='Blog Writer',
+    goal='Narrate compelling tech stories about the video {topic} from YT channel.',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft"
+        " engaging narratives that captivate and educate, bringing new"
+        " discoveries to light in an accessible manner."
+    ),
+    tools=[],
+    allow_delegation=False
+)
